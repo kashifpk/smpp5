7d6d5b18844a2551f555fd791f73b748659fcbbc
asmakomal <komal.bsse1044@iiu.edu.pk>
2013-04-15 21:37:13 +0600

    Update NPI.py

--- smpp5/lib/constants/NPI.py
+++ smpp5/lib/constants/NPI.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # Numberic Plan Indicator (NPI) - SMPP v5.0, section 4.7.2, table 4-43, page 113
 
-UNKNOWN = int('00000000')
-ISDN = int('00000001')
-DATA = int('00000011')
-TELEX = int('00000100')
-LAND_MOBILE = int('00000110')
-NATIONAL = int('00001000')
-PRIVATE = int('00001001')
-ERMES = int('00001010')
-INTERNET = int('00001110')
-WAP_CLIENT_ID = int('00010010')
+UNKNOWN = int(00000000)
+ISDN = int(00000001)
+DATA = int(00000011)
+TELEX = int(00000100)
+LAND_MOBILE = int(00000110)
+NATIONAL = int(00001000)
+PRIVATE = int(00001001)
+ERMES = int(00001010)
+INTERNET = int(00001110)
+WAP_CLIENT_ID = int(00010010)
 
