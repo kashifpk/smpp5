b2c4626c13c663889b52beb6f2d1eaab788cde95
asmakomal <komal.bsse1044@iiu.edu.pk>
2013-04-15 21:31:17 +0600

    Update dest_flag.py

--- smpp5/lib/constants/dest_flag.py
+++ smpp5/lib/constants/dest_flag.py
@@ -1,5 +1,5 @@
 # Dest_Flag- SMPP v5.0, section 4.7.9, table 4-47, page 124
 # Dest_Flag will identify wheather destination address is Distribution List or SME address
 
-SME_ADDRESS = 0x01
-DISTRIBUTION_LIST_NAME = 0x02
+SME_ADDRESS = int(0x01)
+DISTRIBUTION_LIST_NAME = int(0x02)
