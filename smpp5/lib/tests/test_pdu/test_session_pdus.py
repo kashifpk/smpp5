d093260228f635b87ae9cf6b56e95aaf27ed9446
asmakomal <komal.bsse1044@iiu.edu.pk>
2013-04-16 00:45:51 +0600

    Update test_session_pdus.py

--- smpp5/lib/tests/test_pdu/test_session_pdus.py
+++ smpp5/lib/tests/test_pdu/test_session_pdus.py
@@ -39,7 +39,7 @@ def test_01_bind_tr_rsp_encode():
     P.system_id = CString("SMPP3TEST")
     P.interface_version = Integer(IV.SMPP_VERSION_5, 1)
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53' +\
+    assert '00 00 00 2F 80 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53' +\
            '54 00 50 ' == hex_convert(P.encode(), 150)
 
 
@@ -65,7 +65,7 @@ def test_01_bind_rcr_encode():
     P.address_range = CString('')
 
 
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38' + \
+    assert '00 00 00 2F 00 00 00 01 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38' + \
            ' 00 53 55 42 4D 49 54 31 00 50 01 01 00 ' == hex_convert(P.encode(), 150)
 
 
@@ -74,7 +74,7 @@ def test_02_bind_rcr_decode():
     data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01SMPP3TEST\x00secret08\x00' + \
            b'SUBMIT1\x00P\x01\x01\x00'
     P = BindReceiver.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38' + \
+    assert '00 00 00 2F 00 00 00 01 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38' + \
            ' 00 53 55 42 4D 49 54 31 00 50 01 01 00 ' == hex_convert(P.encode(), 150)
 
 #--------------------------------------------------------------------------------
@@ -86,7 +86,7 @@ def test_01_bind_rcr_rsp_encode():
     P.system_id = CString("SMPP3TEST")
     P.interface_version = Integer(IV.SMPP_VERSION_5, 1)
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54' +\
+    assert '00 00 00 2F 80 00 00 01 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54' +\
            '00 50 ' == hex_convert(P.encode(), 150)
 
 
@@ -94,7 +94,7 @@ def test_02_bind_rcr_rsp_decode():
     "Test Bind Receiver Response decoding"
     data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01SMPP3TEST\0x50\x00'
     P = BindReceiverResp.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 ' +\
+    assert '00 00 00 2F 80 00 00 01 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 ' +\
            '00 50 ' == hex_convert(P.encode(), 150)
 
 #--------------------------------------------------------------------------------
@@ -111,16 +111,16 @@ def test_01_bind_tcr_encode():
     P.addr_npi = Integer(NPI.ISDN, 1)
     P.address_range = CString('')
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38' + \
+    assert '00 00 00 2F 00 00 00 09 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38' + \
            ' 00 53 55 42 4D 49 54 31 00 50 01 01 00 ' == hex_convert(P.encode(), 150)
 
 
 def test_02_bind_tcr_decode():
     "Test Bind Transceiver decoding"
-    data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01SMPP3TEST\x00secret08\x00' + \
+    data = b'\x00\x00\x00/\x00\x00\x00\x09\x00\x00\x00\x00\x00\x00\x00\x01SMPP3TEST\x00secret08\x00' + \
            b'SUBMIT1\x00P\x01\x01\x00'
     P = BindTransceiver.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38' + \
+    assert '00 00 00 2F 00 00 00 09 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 30 38' + \
            ' 00 53 55 42 4D 49 54 31 00 50 01 01 00 ' == hex_convert(P.encode(), 150)
 
 #--------------------------------------------------------------------------------
@@ -132,7 +132,7 @@ def test_01_bind_tcr_rsp_encode():
     P.system_id = CString("SMPP3TEST")
     P.interface_version = Integer(IV.SMPP_VERSION_5, 1)
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 ' +\
+    assert '00 00 00 2F 80 00 00 09 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 ' +\
            '54 00 50 ' == hex_convert(P.encode(), 150)
 
 
@@ -140,7 +140,7 @@ def test_02_bind_tcr_rsp_decode():
     "Test Bind Transceiver Response decoding"
     data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01SMPP3TEST\0x50\x00'
     P = BindTransceiverResp.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45' +\
+    assert '00 00 00 2F 80 00 00 09 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45' +\
            '53 54 00 50 ' == hex_convert(P.encode(), 150)
 
 #--------------------------------------------------------------------------------
@@ -152,15 +152,15 @@ def test_01_outbind_encode():
     P.system_id = CString("SMPP3TEST")
     P.password = CString("")
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 ' +\
+    assert '00 00 00 2F 00 00 00 0B 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74 ' +\
            '30 38 00 ' == hex_convert(P.encode(), 150)
 
 
 def test_02_outbind_decode():
     "Test Out Bind decoding"
-    data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01SMPP3TESTsecret08\x00'
+    data = b'\x00\x00\x00/\x00\x00\x00\x0B\x00\x00\x00\x00\x00\x00\x00\x01SMPP3TESTsecret08\x00'
     P = OutBind.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74' +\
+    assert '00 00 00 2F 00 00 00 0B 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65 74' +\
            '30 38 00 ' == hex_convert(P.encode(), 150)
 
 #--------------------------------------------------------------------------------
@@ -169,14 +169,14 @@ def test_01_unbind_encode():
     "Test UnBind encoding"
     P = UnBind()
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01'  == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 00 00 00 06 00 00 00 00 00 00 00 01'  == hex.convert(P.encode(),150)
     
 def test_02_generic_nack_decode():
     "Test UnBind decoding"
     
-    data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01'
+    data = b'\x00\x00\x00/\x00\x00\x00\x06\x00\x00\x00\x00\x00\x00\x00\x01'
     P = UnBind.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 00 00 00 06 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
 
 #--------------------------------------------------------------------------------
 
@@ -184,14 +184,14 @@ def test_01_unbind_rsp_encode():
     "Test UnBindResp encoding"
     P = UnBindResp()
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 80 00 00 06 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
     
 def test_02_unbind_rsp_decode():
     "Test UnBindResp decoding"
     
-    data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01'
+    data = b'\x00\x00\x00/\x00\x00\x00\x06\x00\x00\x00\x00\x00\x00\x00\x01'
     P = UnBindResp.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 80 00 00 06 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
 
 #--------------------------------------------------------------------------------
 
@@ -199,14 +199,14 @@ def test_01_enquirelink_encode():
     "Test EnquireLink encoding"
     P = EnquireLink()
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01'  == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 00 00 00 15 00 00 00 00 00 00 00 01'  == hex.convert(P.encode(),150)
     
 def test_02_enquirelink_decode():
     "Test EnquireLink decoding"
     
-    data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01'
+    data = b'\x00\x00\x00/\x00\x00\x00\x15\x00\x00\x00\x00\x00\x00\x00\x01'
     P = EnquireLink.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 00 00 00 15 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
 
 #--------------------------------------------------------------------------------
 
@@ -214,14 +214,14 @@ def test_01_enquirelink_rsp_encode():
     "Test EnquireLinkResp encoding"
     P = EnquireLinkResp()
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01'  == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 80 00 00 15 00 00 00 00 00 00 00 01'  == hex.convert(P.encode(),150)
     
 def test_02_enquirelink_rsp_decode():
     "Test EnquireLinkResp decoding"
     
-    data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01'
+    data = b'\x00\x00\x00/\x00\x00\x00\x15\x00\x00\x00\x00\x00\x00\x00\x01'
     P = EnquireLinkResp.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 80 00 00 15 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
 
 #--------------------------------------------------------------------------------
 
@@ -241,17 +241,17 @@ def test_01_alert_notification_encode():
     P.ms_availability_status=TLV(1058,0)
    
 
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65' + \
+    assert '00 00 00 2F 00 00 01 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65' + \
            '74 30 38 00 53 55 42 4D 49 54 31 00 00 01 01 01 01 00' == hex.convert(P.encode(),150)
             
             # TLV value missing in it
             
 def test_02_alert_notification_decode():
     "Test Alert Notification decoding"
-    data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01SMPP3TESTsecret08SUBMIT1' + \
+    data = b'\x00\x00\x00/\x00\x00\x01\x02\x00\x00\x00\x00\x00\x00\x00\x01SMPP3TESTsecret08SUBMIT1' + \
            b '\x00\x01\x01\x01\x01\x00'
     P =  AlertNotification.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65' + \
+    assert '00 00 00 2F 00 00 01 02 00 00 00 00 00 00 00 01 53 4D 50 50 33 54 45 53 54 00 73 65 63 72 65' + \
            '74 30 38 00 53 55 42 4D 49 54 31 00 00 01 01 01 01 00' == hex.convert(P.encode(),150)
            
            # TLV value missing in it
@@ -262,13 +262,13 @@ def test_01_generic_nack_encode():
     "Test Generic_Nack encoding"
     P = Generic_Nack()
     
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01'  == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 80 00 00 00 00 00 00 00 00 00 00 01'  == hex.convert(P.encode(),150)
     
 def test_02_generic_nack_decode():
     "Test Generic_Nack decoding"
     
     data = b'\x00\x00\x00/\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x01'
     P = Generic_Nack.decode(data)
-    assert '00 00 00 2F 00 00 00 02 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
+    assert '00 00 00 2F 80 00 00 00 00 00 00 00 00 00 00 01' == hex.convert(P.encode(),150)
     
 #--------------------------------------------------------------------------------
