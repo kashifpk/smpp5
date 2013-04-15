396d21cef427602895022e391f2d1f4b0cd20d6a
asmakomal <komal.bsse1044@iiu.edu.pk>
2013-04-15 14:05:12 +0600

    Update session_management.py

--- smpp5/lib/pdu/session_management.py
+++ smpp5/lib/pdu/session_management.py
@@ -30,14 +30,14 @@ class BindTransmitter(PDU):
     addr_npi = Integer(NPI.ISDN, 1) # Numbering Plan Indicator for ESME address. If not known set to NULL.
     address_range = CString('')     # Numbering Plan Indicator for ESME address. If not known set to NULL.
 
-class bind_transmitter_resp(PDU):
+class BindTransmitterResp(PDU):
     "The SMPP bind_transmitter_resp PDU is used to reply to a bind_transmitter request"
     
     command_id = Integer(command_id.bind_transmitter_resp, 4)
     system_id = CString("")         # Message Center(MC) identifier.Identifies the MC to the ESME.
     sc_interface_version = Integer(IV.SMPP_VERSION_5, 1) # sc_interface_version
     
-class bind_receiver(PDU):
+class BindReceiver(PDU):
     "SMPP bind_receiver PDU type"
     
     command_id = Integer(command_id.bind_receiver, 4)
@@ -49,14 +49,14 @@ class bind_receiver(PDU):
     addr_npi = Integer(NPI.ISDN, 1) # Numbering Plan Indicator (NPI) for ESME address(es) served via this SMPP receiver session.Set to NULL if not known.   
     address_range = CString('')     # A single ESME address or a range of ESME addresses served via this SMPP receiver session.Set to NULL if not known.
     
-class bind_receiver_resp(PDU):
+class BindReceiverResp(PDU):
     "SMPP bind_receiver_resp PDU type"
     
     command_id = Integer(command_id.bind_receiver_resp, 4)
     system_id = CString("")         # Message Center(MC) identifier.Identifies the MC to the ESME.
     sc_interface_version = Integer(IV.SMPP_VERSION_5, 1) # SMPP version supported by MC
     
-class bind_transceiver(PDU):
+class BindTransceiver(PDU):
     "SMPP bind_transceiver PDU type"
     
     command_id = Integer(command_id.bind_transceiver, 4)
@@ -68,14 +68,14 @@ class bind_transceiver(PDU):
     addr_npi = Integer(NPI.ISDN, 1) # Numbering Plan Indicator (NPI) for ESME address(es) served via this SMPP transceiver session. Set to NULL (Unknown) if not known.
     address_range = CString('')     # A single ESME address or a range of ESME addresses served via this SMPP transceiver session. Set to NULL if not known.
     
-class bind_transceiver_resp(PDU):
+class BindTransceiverResp(PDU):
     "SMPP bind_transceiver_resp type"
     
     command_id = Integer(command_id.bind_transceiver_resp, 4)
     system_id = CString("")         # MC identifier. Identifies the MC to the ESME.
     interface_version = Integer(IV.SMPP_VERSION_5, 1) # Identifies the version of the SMPP protocol supported by the ESME.
     
-class outbind(PDU):
+class Outbind(PDU):
     '''SMPP outbind PDU type
     This operation is used by the MC to signal an ESME to originate a outbind request to the MC.'''
     
@@ -83,7 +83,7 @@ class outbind(PDU):
     system_id = CString("")         # MC identifier. Identifies the MC to the ESME.
     password = CString("")          # The password may be used by the ESME for security reasons to authenticate the MC originating the outbind.
     
-class unbind(PDU):
+class Unbind(PDU):
     '''SMPP unbind PDU type
     The purpose of the SMPP unbind operation is to deregister an instance of an ESME from the MC 
     and inform the MC that the ESME no longer wishes to use this network connection for
@@ -93,7 +93,7 @@ class unbind(PDU):
 
     command_id = Integer(command_id.unbind, 4)
     
-class unbind_resp(PDU):
+class UnbindResp(PDU):
     '''SMPP unbind_resp PDU type
     The SMPP unbind_resp PDU is used to reply to an unbind request. It comprises the SMPP
     message header only.'''
@@ -107,13 +107,13 @@ receiving party should respond with an enquire_link_resp, thus verifying that th
 level connection between the MC and the ESME is functioning. The ESME may also respond
 by sending any valid SMPP primitive.'''
     
-class enquire_link(PDU):
+class EnquireLink(PDU):
     '''This PDU can be originated by either the ESME or MC and is used to provide a confidencecheck
     of the communication path between an ESME and a MC.'''
     
     command_id = Integer(command_id.enquire_link, 4)
     
-class enquire_link_resp(PDU):
+class EnquireLinkResp(PDU):
     '''The enquire_link_resp PDU is used to reply to an enquire_link request.'''
     
     command_id = Integer(command_idenquire_link_resp, 4)
@@ -128,7 +128,7 @@ A typical use of this operation is to trigger a data content ‘Push’ to the s
 Proxy Server.
 Note: There is no associated alert_notification_resp PDU.'''
 
-class alert_notification(PDU):
+class AlertNotification(PDU):
     '''SMPP alert_notification PDU type'''
     
     command_id = Integer(command_id.alert_notification, 4)
@@ -144,7 +144,7 @@ class alert_notification(PDU):
 The generic_nack PDU is used to acknowledge the submission of an unrecognized or
 corrupt PDU.'''
 
-class generic_nack(PDU):
+class GenericNack(PDU):
     "SMPP generic_nack PDU type"
     
     command_id = Integer(command_id.generic_nack, 4)
