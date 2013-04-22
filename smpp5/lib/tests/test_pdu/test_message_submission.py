89cdf1d091f2abb586cc94602eead0ad0b5ae137
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-04-22 09:58:38 -0700

    Update test_message_submission.py

--- smpp5/lib/tests/test_pdu/test_message_submission.py
+++ smpp5/lib/tests/test_pdu/test_message_submission.py
@@ -40,20 +40,46 @@ def test_26_sbmit_sm_decode():
  #-----------------------------------------------------------------------
  
 def test_01_sbmit_sm_resp_encode():
-    "Test Submit Sm Response Encoding"
+    "Test Submit Sm Encoding"
     P = SubmitSmResp()
     P.message_id = CString("2468ACE")
     assert '00 00 00 18 80 00 00 04 00 00 00 00 00 00 00 01 32 34 36 38 41 43 45 00 ' ==  hex_convert(P.encode(), 150)
     
     
 def test_01_sbmit_sm_resp_decode():
-    "Test Submit Sm Response Decoding"
+    "Test Submit Sm Decoding"
     
     data = b'\x00\x00\x00\x18\x80\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x012468ACE\x00'
 
     P = SubmitSmResp.decode(data)
     assert '00 00 00 18 80 00 00 04 00 00 00 00 00 00 00 01 32 34 36 38 41 43 45 00 ' ==  hex_convert(P.encode(), 150)
+    
+#---------------------------------------------------------------------------
 
+def test_01_data_sm_encode():
+    "Test Data Sm Encoding"
+    
+    P=DataSm()
+
+    P.command_id = Integer(command_id.data_sm, 4)
+    P.source_addr_ton = Integer(TON.INTERNATIONAL, 1)
+    P.source_addr_npi = Integer(NPI.ISDN, 1)
+    P.source_addr = CString("1616")
+    P.dest_addr_ton = Integer(TON.INTERNATIONAL, 1)
+    P.dest_addr_npi = Integer(NPI.ISDN, 1)
+    P.destination_addr = CString("1515")
+    P.esm_class = Integer(esm_class.Default_mode, 1)  
+    P.registered_delievery = Integer(0, 1)     
+    P.data_coding = Integer(0, 1)
+    
+    assert '00 00 00 22 00 00 01 03 00 00 00 00 00 00 00 01 00 01 01 31 36 31 36 00 01 01 31 35 31 35 00 00 00 00 ' ==  hex_convert(P.encode(), 150)
+
+    
+def test_01_data_sm_decode():
+    "Test Data Sm Decoding"
+    data =  b'\x00\x00\x00\x22\x00\x00\x01\x03\x00\x00\x00\x00\x00\x00\x00\x01\x00\x01\x011616\x00\x01\x011515\x00\x00\x00\x00'
+    P = DataSm.decode(data)
+    assert '00 00 00 22 00 00 01 03 00 00 00 00 00 00 00 01 00 01 01 31 36 31 36 00 01 01 31 35 31 35 00 00 00 00 ' ==  hex_convert(P.encode(), 150)
 
     
     
