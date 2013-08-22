bc1308f46f56c3125794bb0115d04902807d7b94
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-08-22 23:29:53 +0500

    Update smpp_client.py

--- smpp5/smpp5/client/smpp_client.py
+++ smpp5/smpp5/client/smpp_client.py
@@ -13,7 +13,8 @@ class SMPPClient(object):
     password = None
     system_type = None
     session = None
-    status = None
+    validation_status = None
+    conn_status = 'noconn'
 
     def __init__(self):
         pass
@@ -24,21 +25,33 @@ class SMPPClient(object):
             self.socket.connect((host, port))
             self.session = SMPPSession('client', self.socket)
             print("Connection established successfully\n")
-            self.status = 'success'
+            self.conn_status = 'connected'
         except:
             print("Connection Refused...Try Again\n")
 
     def disconnect(self):
         #TODO: close SMPPSession if not already closed
-        self.socket.close()
+        if(self.conn_status == 'connected'):
+            self.socket.close()
 
     def login(self, mode, system_id, password, system_type):
-        self.session.bind(mode, system_id, password, system_type)
-        self.status = self.session.status
+        if(self.conn_status == 'connected'):
+            self.session.bind(mode, system_id, password, system_type)
+            self.validation_status = self.session.status
+            if(self.validation_status != 'success'):
+                print("Oops!!validation failed")
         #self.session.handle_response()
 
+    def send_sms(self, recipient, message, system_id):
+        if(self.conn_status == 'connected'):
+            message_id = self.session.send_sms(recipient, message, system_id)
+            print("Message id of Message U have just sent is "+message_id)
+
+    def query_status(self, message_id):
+        self.session.query_status(message_id)
+
     def logout(self):
-        if(self.status == 'success'):
+        if(self.conn_status == 'connected'):
             self.session.close()
 
     #def logoff(self):
@@ -52,5 +65,6 @@ if __name__ == '__main__':
     client = SMPPClient()
     client.connect('127.0.0.1', 1337)
     client.login('TX', '3TEST', 'secret08', 'SUBMIT1')
+    client.send_sms('+923005381993', 'hello from asma project yessssss :-)', '3TEST')
     client.logout()
     client.disconnect()
