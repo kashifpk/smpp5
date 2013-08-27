3b97706adccd3e346b6ed4f25c740ca690e4145a
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-08-27 18:41:29 +0500

    Update models.py

--- smpp5web/smpp5web/models/models.py
+++ smpp5web/smpp5web/models/models.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy import (
     Column,
     Integer,
@@ -23,9 +24,8 @@ class Sms(Base):
     status = Column(Unicode(40))
     msg_type = Column(Unicode(40))
     user_id = Column(Unicode(100), ForeignKey(User.user_id))
- 
-    def __init__(self, sms_id=None, sms_type=None, sms_from=None, sms_to=None, msg=None, timestamp = None, status=None, msg_type=None, user_id=None):
-        self.sms_id = sms_id
+
+    def __init__(self, sms_type=None, sms_from=None, sms_to=None, msg=None, timestamp=None, status=None, msg_type=None, user_id=None):
         self.sms_type = sms_type
         self.sms_from = sms_from
         self.sms_to = sms_to
@@ -38,7 +38,12 @@ class Sms(Base):
 
 class User_Number(Base):
     __tablename__ = 'user_number'
-    
-    user_id = Column(Integer, primary_key=True)
-    cell_number = Column(Integer, primary_key=True)           
+
+    id = Column(Integer, primary_key=True)
+    cell_number = Column(Unicode(40), primary_key=True)
     user_id = Column(Unicode(100), ForeignKey(User.user_id))
+
+    def __init__(self, cell_number=None, user_id=None):
+        self.cell_number = cell_number
+        self.user_id = user_id
+        
