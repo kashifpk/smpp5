da855c7a1913cf0ab72b70c9a2967203a6d39184
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-09-05 16:05:41 +0500

    2 fiels are added to sms body

--- smpp5/smpp5/send_sms.py
+++ smpp5/smpp5/send_sms.py
@@ -2,6 +2,8 @@ from bottle import *
 import android, socket, struct
 
 def return_ip():
+  '''This method is responsible for getting ip address
+    '''
     droid = android.Android()
     ipdec = droid.wifiGetConnectionInfo().result['ip_address']
     ipstr = socket.inet_ntoa(struct.pack('L', ipdec))
@@ -9,14 +11,15 @@ def return_ip():
 
 
 @post('/sendsms')
-def read_sms():
-    '''This method is responsible for reading sms from url to which smpp server has posted and send to the particular recipient
+'''This method is responsible for reading sms from url to which smpp server has posted and send to the particular recipient
     '''
-    
-    number = request.forms.number
+def read_sms():
+    sms_to = request.forms.sms_to
+    sms_from = request.forms.sms_from
     message = request.forms.message 
+    sms_body = "Sms To :"+sms_to +"\nSms From :"+sms_from +"\nMessage :" +message
     droid = android.Android()
-    droid.smsSend(number,message)
+    droid.smsSend(sms_to,sms_body)
     return "Message sent successfully"
 
 #Running at this host and port on android
