d48515810241191fe940c0e5b60c76b890d79182
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-09-05 16:29:49 +0500

    check cell number against mapping tables

--- smpp5web/smpp5web/controllers/controllers.py
+++ smpp5web/smpp5web/controllers/controllers.py
@@ -3,7 +3,10 @@ from pyramid.httpexceptions import HTTPFound
 import datetime
 
 from ..models import (
-    DBSession, Sms)
+    DBSession, Sms, User_Number, Prefix_Match)
+
+from ..auth import (User)
+
 
 from ..forms import ContactForm
 
@@ -32,16 +35,32 @@ def contact_form(request):
 @view_config(route_name='sms_in', renderer='sms.mako')
 def say(request):
     if "POST" == request.method:
+        sms_body = request.POST['body']
+        sms_to = sms_body.splitlines()[0]
+        sms_from = request.POST['address']
+        message = sms_body.splitlines()[1]
+        #Making Instance of Sms and Saving values in db
         S = Sms()
         S.sms_type = 'incoming'
-        S.sms_from = request.POST['address']
-        S.sms_to = 'None'
-        S.msg = request.POST['body']
+        S.sms_from = sms_from
+        S.sms_to = sms_to
+        S.msg = message
+        user_number = DBSession.query(User_Number).filter_by(cell_number=sms_to).first()
+        # if number in sms_to is not present in user_number table then find number in telecom table
+        if(user_number is None):
+            prefix = '0'+sms_to[3:6]
+            telecom_number = DBSession.query(Prefix_Match).filter_by(prefix=prefix).first()
+            if(telecom_number is not None):
+                users = DBSession.query(User).filter_by(user_id=telecom_number.user_id).first()
+                if(users.bind_account_type == 'telecom'):
+                    S.user_id = telecom_number.user_id
+        else:
+            S.user_id = user_number.user_id
         S.timestamp = datetime.datetime.now()
         S.status = 'pending'
         S.msg_type = 'text'
-        S.user_id = '3TEST'
-        DBSession.add(S)
+        if(user_number or telecom_number is not None):
+            DBSession.add(S)
         return{}
 
 
