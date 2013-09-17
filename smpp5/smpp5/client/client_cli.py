a86e5b936c7a10ed973f0c0c3f8b164abef6b560
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-09-17 17:02:33 +0500

    Update client_cli.py

--- smpp5/smpp5/client/client_cli.py
+++ smpp5/smpp5/client/client_cli.py
@@ -15,35 +15,41 @@ def ui_loop(client):
         print("Press 2 to query the status of previously submitted short Text Message")
         print("Press 3 to cancel a previously submitted Short Text Message")
         print("Press 4 to replace a previously submitted Short Text Message")
-        print("Press 5 to exit")
+        print("Press 5 to view Unread Smses")
+        print("Press 6 to exit")
         option = int(input())
         if(option == 1):
             recipient = input("Enter the Recipient                                   ")
             message = input("Enter the Short Message to send      ")
             if not recipient.startswith('+'):
                 recipient = '+92' + recipient[1:]
-
-            client.send_sms(recipient, message)
+            client.session.send_sms(recipient, message)
 
         elif(option == 2):
             message_id = input("Enter the Message Id of Message whom Status is required    ")
-            self.client.query_status(message_id)
+            client.session.query_status(message_id)
+
         elif(option == 3):
             message_id = system_id = input("Enter the Message Id of Message whom you want to cancel    ")
-            self.client.cancel_sms(message_id)
+            client.session.cancel_sms(message_id)
+
         elif(option == 4):
             message_id = input("Enter the Message Id of Message whom you want to replace    ")
-            self.message = input("Enter the Short Message to replace previous sumbitted short message      ")
-            self.client.replace_sms(message_id, self.message,)
+            message = input("Enter the Short Message to replace previous sumbitted short message      ")
+            client.session.replace_sms(message_id, message,)
+
         elif(option == 5):
+            pass
+
+        elif(option == 6):
             break
+
         else:
             print("\nInvalid Option......")
 
-
-        self.client.logout()
-        self.client.disconnect()
-        print("Thank You.....Good Bye!!")
+    client.session.unbind()
+    client.socket.close()
+    print("Thank You.....Good Bye!!")
 
 
 def get_connect_info():
@@ -55,8 +61,7 @@ def get_connect_info():
         print("**** Select Bind Type ****")
         bind_types = ['TX', 'RX', 'TRX']
         for bt in bind_types:
-            print("%i. %s" % (bind_types.index(bt), bt)
-
+            print("%i. %s" % (bind_types.index(bt), bt))
         option = int(input())
         try:
             ret['bind_type'] = bind_types[option]
@@ -71,22 +76,28 @@ def get_connect_info():
         return ret
 
 if __name__ == '__main__':
+
     conn_info = get_connect_info()
 
-    client = SMPPClient(conn_info['ip'], conn_info['port'], conn_info['bind_type'],
-                        conn_info['system_id'], conn_info['password'], conn_info['system_type'])
+    try:
+        client = SMPPClient(conn_info['ip'], conn_info['port'], conn_info['bind_type'],
+                            conn_info['system_id'], conn_info['password'], conn_info['system_type'])
 
-    if client.connect():
-        print("Connection established successfully\n")
-    else:
-        print("Connection Refused...Try Again\n")
-        sys.exit()
+        if client.connect():
+            print("Connection established successfully\n")
+        else:
+            print("Connection Refused...Try Again\n")
+            sys.exit()
 
-    if client.login():
-        print("Login successful\n")
-    else:
-        print("Login Failed")
-        sys.exit()
+        if client.login():
+            print("Login successful\n")
+            #to check if ui_loop method is functioning correct
+            ui_loop(client)
+        else:
+            print("Login Failed")
+            sys.exit()
+    except:
+        print("Try Again.....")
 
     # if here then login was successful, now create a background thread for handling traffic from server
     # and run UI loop in the main process/thread for user interaction
@@ -95,5 +106,3 @@ if __name__ == '__main__':
     # ready to read from socket periodically and sleep in the meanwhile (non-blocking). We need to go with the non-blocking
     # option since socket is shared between both threads and if it is blocked in socket.recv in the background thread we
     # cannot send data through it in the UI thread.
-
-
