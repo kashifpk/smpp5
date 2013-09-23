bba3599266b552fdb1e41364b2ebd95ea189f3bc
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-09-24 00:13:42 +0500

    models added

--- smpp5web/smpp5web/models/models.py
+++ smpp5web/smpp5web/models/models.py
@@ -1,6 +1,8 @@
+
 from sqlalchemy import (
     Column,
     Integer,
+    Float,
     Unicode,
     ForeignKey,
     DateTime)
@@ -9,6 +11,21 @@ from . import DBSession, Base
 from . auth import User
 
 # Create your models here.
+
+
+class Packages(Base):
+    __tablename__ = 'packages'
+
+    package_name = Column(Unicode(50), primary_key=True)
+    rates = Column(Float)
+    smses = Column(Integer)
+    duration = Column(Unicode(100))
+
+    def __init__(self, package_name=None, rates=None, smses=None, duration=None):
+        package_name = self.package_name
+        rates = self.rates
+        smses = self.smses
+        duration = self.duration
 
 
 class Sms(Base):
@@ -23,8 +40,11 @@ class Sms(Base):
     status = Column(Unicode(40))
     msg_type = Column(Unicode(40))
     user_id = Column(Unicode(100), ForeignKey(User.user_id))
+    package_name = Column(Unicode(50), ForeignKey(Packages.package_name))
+    rates = Column(Float)
 
-    def __init__(self, sms_type=None, sms_from=None, sms_to=None, msg=None, timestamp=None, status=None, msg_type=None, user_id=None):
+    def __init__(self, sms_type=None, sms_from=None, sms_to=None, msg=None, timestamp=None, status=None, msg_type=None,
+                 user_id=None, package_name=None, rates=None):
         self.sms_type = sms_type
         self.sms_from = sms_from
         self.sms_to = sms_to
@@ -33,6 +53,8 @@ class Sms(Base):
         self.status = status
         self.msg_type = msg_type
         self.user_id = user_id
+        self.package_name = package_name
+        self.rates = rates
 
 
 class User_Number(Base):
@@ -45,13 +67,48 @@ class User_Number(Base):
         self.user_id = user_id
         self.cell_number = cell_number
 
+
 class Prefix_Match(Base):
     __tablename__ = 'prefix_match'
-    
-    prefix = Column(Unicode(4), primary_key=True)           
+
+    prefix = Column(Unicode(4), primary_key=True)
     user_id = Column(Unicode(100), ForeignKey(User.user_id))
-        
+
     def __init__(self, prefix=None, user_id=None):
         self.prefix = prefix
         self.user_id = user_id
-    
+
+
+class Selected_package(Base):
+    __tablename__ = 'selected_package'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Unicode(100), ForeignKey(User.user_id))
+    package_name = Column(Unicode(50), ForeignKey(Packages.package_name))
+    smses = Column(Integer)
+    rates = Column(Float)
+    start_date = Column(DateTime)
+    end_date = Column(DateTime)
+    status = Column(Unicode(100))
+
+    def __init__(self, user_id=None, package_name=None, smses=None, rates=None, start_date=None, end_date=None,
+                 status=None):
+        package_name = self.package_name
+        smses = self.smses
+        rates = self.rates
+        start_date = self.start_date
+        end_date = self.end_date
+        status = self.status
+
+
+class Rates(Base):
+    __tablename__ = 'rates'
+
+    id = Column(Integer, primary_key=True)
+    network_type = Column(Unicode(50))
+    rates = Column(Float)
+
+    def __init__(self, network_type=None, rates=None):
+        network_type = self.network_type
+        rates = self.rates
+
