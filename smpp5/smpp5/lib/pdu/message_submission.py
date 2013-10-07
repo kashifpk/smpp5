9af5ea70a80e4efa87c7ec1617211190e244a07e
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-10-07 15:04:44 +0500

    Update message_submission.py

--- smpp5/smpp5/lib/pdu/message_submission.py
+++ smpp5/smpp5/lib/pdu/message_submission.py
@@ -21,12 +21,11 @@ class SubmitSm(PDU):
     schedule_delivery_time = CString("")
     validity_period = CString("")
     registered_delievery = Integer(0, 1)     # page 130
-    replace_if_present_flag = Integer(0, 1)  # page 131
+    replace_if_present_flag = Integer(1, 1)  # page 131
     data_coding = Integer(0, 1)              # page 123
     sm_default_msg_id = Integer(0, 1)        # page 134
     sm_length = Integer(0, 1)                # page 134
     short_message = CString("")
-    #TLV Submission operations
 
 
 class SubmitSmResp(PDU):
@@ -87,7 +86,7 @@ class SubmitMulti(PDU):
     data_coding = Integer(0, 1)              # page 123
     sm_default_msg_id = Integer(0, 1)        # page 134
     sm_length = Integer(0, 1)                # page 134
-    short_message = String("")
+    short_message = CString("")
     #TLV Submission operations
 
 
@@ -104,99 +103,3 @@ class SubmitMultiResp(PDU):
     #message submission response TLV
 
 
-class QuerySm(PDU):
-    "Query Submission PDU type"
-
-    command_id = Integer(command_ids.query_sm, 4)
-    message_id = CString("")
-    source_addr_ton = Integer(TON.INTERNATIONAL, 1)
-    source_addr_npi = Integer(NPI.ISDN, 1)
-    source_addr = CString("")
-
-
-class QuerySmResp(PDU):
-    "Query Submission Response PDU type"
-
-    command_id = Integer(command_ids.query_sm_resp, 4)
-    message_id = CString("")
-    final_date = CString("")
-    message_state = Integer(0, 1)
-    error_code = Integer(0, 1)
-
-
-class CancelSm(PDU):
-    "Cancel Message Submission PDU type"
-
-    command_id = Integer(command_ids.cancel_sm, 4)
-    service_type = CString("")
-    message_id = CString("")
-    source_addr_ton = Integer(TON.INTERNATIONAL, 1)
-    source_addr_npi = Integer(NPI.ISDN, 1)
-    source_addr = CString("")
-    dest_addr_ton = Integer(TON.INTERNATIONAL, 1)
-    dest_addr_npi = Integer(NPI.ISDN, 1)
-    destination_addr = CString("")
-
-
-class CancelSmResp(PDU):
-    "Cancel Message Response PDU type"
-
-    command_id = Integer(command_ids.cancel_sm_resp, 4)
-
-
-class ReplaceSm(PDU):
-    "Replace Short Message PDU type"
-
-    command_id = Integer(command_ids.replace_sm, 4)
-    message_id = CString("")
-    source_addr_ton = Integer(TON.INTERNATIONAL, 1)
-    source_addr_npi = Integer(NPI.ISDN, 1)
-    source_addr = CString("")
-    schedule_delivery_time = CString("")
-    validity_period = CString("")
-    registered_delievery = Integer(0, 1)
-    sm_default_msg_id = Integer(0, 1)
-    sm_length = Integer(0, 1)                # page 134
-    short_message = CString("")              # This field should be CString
-
-
-class ReplaceSmResp(PDU):
-    "Replace Short Message Response PDU type"
-
-    command_id = Integer(command_ids.replace_sm_resp, 4)
-
-
-class DeliverSm(PDU):
-    "Deliver Short Message PDU type"
-
-    command_id = Integer(command_ids.deliver_sm, 4)
-    service_type = CString("")
-    source_addr_ton = Integer(TON.INTERNATIONAL, 1)
-    source_addr_npi = Integer(NPI.ISDN, 1)
-    source_addr = CString("")
-    dest_addr_ton = Integer(TON.INTERNATIONAL, 1)
-    dest_addr_npi = Integer(NPI.ISDN, 1)
-    destination_addr = CString("")
-    esm_class = Integer(esm_class.Default_mode, 1)
-    protocol_id = Integer(0, 1)              # page 129, its value is NULL
-    priority_flag = Integer(0, 1)            # page 129
-    schedule_delivery_time = CString("")
-    validity_period = CString("")
-    registered_delievery = Integer(0, 1)     # page 130
-    replace_if_present_flag = Integer(0, 1)  # page 131
-    data_coding = Integer(0, 1)              # page 123
-    sm_default_msg_id = Integer(0, 1)        # page 134
-    sm_length = Integer(0, 1)                # page 134
-    short_message = String("")
-    #TLV Submission operations
-
-
-class DeliverSmResp(PDU):
-    "Deliver Short Message Response PDU type"
-    
-    command_id = Integer(command_ids.deliver_sm_resp, 4)
-    message_id = CString("")
-    #message deliver response TLV
-
-
-    
