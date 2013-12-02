bd2afe08bbe8860c4c915ebf3ea792cfc97675da
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-12-02 22:21:09 +0500

    object of Sharedconnection class is created

--- smpp5/smpp5/client/smpp_client.py
+++ smpp5/smpp5/client/smpp_client.py
@@ -1,6 +1,10 @@
 import socket
 from smpp5.lib.session import SMPPSession, SessionState
 from smpp5.lib.constants import NPI, TON, esm_class, command_ids, command_status, tlv_tag, message_state
+from smpp5.client.cli_background_thread import client_thread
+from smpp5.client import cli_background_thread
+from smpp5.lib.session.shared_connection import SharedConnection
+import time
 
 
 # Note: This whole module seems mostly unnecessary. We either need this or client_cli module, not both.
@@ -18,6 +22,7 @@ class SMPPClient(object):
     system_type = None
     bind_mode = None
     session = None
+    sc = None
 
     def __init__(self, ip, port, bind_mode, system_id, password, system_type):
         self.ip = ip
@@ -31,7 +36,9 @@ class SMPPClient(object):
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.connect((self.ip, self.port))
-            self.session = SMPPSession('client', self.socket)
+            self.sc = SharedConnection(self.socket)
+            self.sc.is_open = True
+            self.session = SMPPSession('client', self.sc)
         except:
             return False
 
@@ -41,6 +48,8 @@ class SMPPClient(object):
         ret = False
         if SessionState.OPEN == self.session.state:
             self.session.bind(self.bind_mode, self.system_id, self.password, self.system_type)
+            time.sleep(1)
+            self.session.processing_recieved_pdus()
             if self.session.state in [SessionState.BOUND_RX, SessionState.BOUND_TX, SessionState.BOUND_TRX]:
                 return True
 
