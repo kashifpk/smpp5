d4cb614ff6c65e058635739941ac802cd0fe3bff
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-12-11 17:32:48 +0500

    Update pc_send_sms.py

--- smpp5/smpp5/server/pc_send_sms.py
+++ smpp5/smpp5/server/pc_send_sms.py
@@ -7,27 +7,27 @@ import db
 import transaction
 from db import DBSession
 from models import Sms, User
+import datetime
 
 try:
     db.bind_session()
-    smses = DBSession.query(Sms).filter_by(status='scheduled', sms_type='outgoing').count()
-    if(smses == 0):
-        print("\nYou have no pending messages to sent.......Thanks\n")
-    else:
-        smses = DBSession.query(Sms).filter_by(status='scheduled', sms_type='outgoing').all()
-        for S in smses:
-            sms_to = S.sms_to
-            sms_from = S.sms_from
-            message = S.msg
-            post_data = {'sms_to': sms_to, 'sms_from': sms_from, 'message': message}
-            post_str = urllib.urlencode(post_data)    
+    while True:
+        smses = DBSession.query(Sms).filter_by(status='scheduled', sms_type='outgoing', target_network='ufone').all()
+        if(smses):
+            for S in smses:
+                sms_to = S.sms_to
+                sms_from = S.sms_from
+                message = S.msg
+                post_data = {'sms_to': sms_to, 'sms_from': sms_from, 'message': message}
+                post_str = urllib.urlencode(post_data)
             # urlencode converts the dict to a string suitable for POST
             # Note when some data is specified in urlopen then HTTP request type 
             # POST is used instead of GET request type
-            result = urllib2.urlopen("http://192.168.1.2:50111/" + 'sendsms', post_str).read()
-            S.status = 'delivered'
-            print(result)
-        transaction.commit()
+                result = urllib2.urlopen("http://192.168.1.2:50111/" + 'sendsms', post_str).read()
+                S.status = 'delivered'
+                S.timestamp = datetime.datetime.now()
+                print(result)
+            transaction.commit()
 
 except Exception, e:
     print(e.__class__.__name__, e.message)
