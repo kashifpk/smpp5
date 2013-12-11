02590723ba107f8599a16dbb8fbed5d4d2b9616f
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-12-11 17:10:13 +0500

    Update client_cli.py

--- smpp5/smpp5/client/client_cli.py
+++ smpp5/smpp5/client/client_cli.py
@@ -4,7 +4,7 @@ SMPP CLIENT INTERFACE
 
 import sys
 import time
-from smpp5.lib.session import SMPPSession
+from smpp5.lib.session import SMPPSession, SessionState
 from smpp5.client.smpp_client import SMPPClient
 import threading
 
@@ -18,14 +18,15 @@ def ui_loop(client):
         if(notification == 0):
             pass
         else:
-            print("* You have pending  notifications...Press 6 to view them....thank you....")
+            print("*** You have pending  notifications...Press 6 to view them....")
         print("\nPress 1 to send Short Text Message")
         print("Press 2 to query the status of previously submitted short Text Message")
         print("Press 3 to cancel a previously submitted Short Text Message")
         print("Press 4 to replace a previously submitted Short Text Message")
         print("Press 5 to view Unread Smses")
         print("Press 6 to view pending notifications")
-        print("Press 7 to exit")
+        print("Press 7 to check connectivity with server")
+        print("Press 8 to exit")
         option = int(input())
         if(option == 1):
             recipient = input("Enter the Recipient                                   ")
@@ -35,38 +36,49 @@ def ui_loop(client):
             client.session.send_sms(recipient, message)
 
         elif(option == 2):
-            message_id = input("Enter the Message Id of Message whose Status is required    ")
+            message_id = input("Enter the Message Id of Message whom Status is required    ")
             client.session.query_status(message_id)
 
         elif(option == 3):
-            message_id = input("Enter the Message Id of Message which you want to cancel    ")
+            message_id = system_id = input("Enter the Message Id of Message whom you want to cancel    ")
             client.session.cancel_sms(message_id)
 
         elif(option == 4):
-            message_id = input("Enter the Message Id of Message which you want to replace    ")
+            message_id = input("Enter the Message Id of Message whom you want to replace    ")
             message = input("Enter the Short Message to replace previous sumbitted short message      ")
             client.session.replace_sms(message_id, message)
 
         elif(option == 5):
             client.session.view_smses()
 
+            key = input("\nPress Enter To Continue.......")
+            while key != '':
+                key = input("Press Enter To Continue.......")
+
         elif(option == 6):
             count = client.session.notifications_4_client()
             if(count == 0):
-                print("You have no pending notifications...")
+                print("You have no any pending notification...")
             else:
                 while count > 0:
                     client.session.processing_recieved_pdus()
                     count = count - 1
 
+            key = input("\nPress Enter To Continue.......")
+            while key != '':
+                key = input("\nPress Enter To Continue.......")
+
         elif(option == 7):
+            client.session.enquire_link()
+
+        elif(option == 8):
             break
 
         else:
-            print("\nInvalid Selection......")
+            print("\nInvalid Option......")
     client.session.unbind()
     time.sleep(1)
-    while client.session.state != 5:
+    while client.session.state != SessionState.UNBOUND:
         client.session.processing_recieved_pdus()
     client.sc.close()
     print("Thank You.....Good Bye!!")
@@ -96,33 +108,29 @@ def get_connect_info():
         return ret
 
 
-def client_thread(client):
-    while client.sc.is_open is True: #if shared socket and shared connection is open 
-        client.session.storing_recieved_pdus()#call this method of session class via the object passed as parameter
-
 if __name__ == '__main__':
 
     conn_info = get_connect_info()
 
     client = SMPPClient(conn_info['ip'], conn_info['port'], conn_info['bind_type'],
-                        conn_info['system_id'], conn_info['password'], conn_info['system_type'])#passing connection information to client object
+                        conn_info['system_id'], conn_info['password'], conn_info['system_type'])
 
     if client.connect():
         print("Connection established successfully\n")
-        background_thread = threading.Thread(target=client_thread, args=(client,))#creating a background thread for the client and giving name of method in target for execution
-        background_thread.start()#starting the background thread
+        background_thread = threading.Thread(target=client.session.storing_recieved_pdus, args=())
+        background_thread.start()
     else:
         print("Connection Refused...Try Again\n")
         sys.exit()
 
     if client.login():
         print("Login successful\n")
-        ui_loop(client)#to check if ui_loop method is functioning correct
-        print("Yes it is...")
-        background_thread.join()#to assure that main thread should exit when background has completed all its operations
+        ui_loop(client)  # to check if ui_loop method is functioning correct
+        background_thread.join()
     else:
         print("Login Failed")
-        background_thread.join()#to assure that main thread should exit when background has completed all its operations
+        client.sc.close()
+        background_thread.join()
         sys.exit()
 
     # if here then login was successful, now create a background thread for handling traffic from server
