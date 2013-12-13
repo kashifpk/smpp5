f2ebbf08281c3de40361fc9f0ef6be2f3c2c17b5
Asma Mahmood Abbasi <mahmood.abbasi08@yahoo.com>
2013-12-13 15:52:16 +0500

    Update pc_send_sms.py

--- smpp5/smpp5/server/pc_send_sms.py
+++ smpp5/smpp5/server/pc_send_sms.py
@@ -20,14 +20,14 @@ try:
                 message = S.msg
                 post_data = {'sms_to': sms_to, 'sms_from': sms_from, 'message': message}
                 post_str = urllib.urlencode(post_data)
-            # urlencode converts the dict to a string suitable for POST
-            # Note when some data is specified in urlopen then HTTP request type 
-            # POST is used instead of GET request type
+                # urlencode converts the dict to a string suitable for POST
+                # Note when some data is specified in urlopen then HTTP request type 
+                # POST is used instead of GET request type
                 result = urllib2.urlopen("http://192.168.1.2:50111/" + 'sendsms', post_str).read()
                 S.status = 'delivered'
                 S.timestamp = datetime.datetime.now()
                 print(result)
-            transaction.commit()
+        transaction.commit()
 
 except Exception, e:
     print(e.__class__.__name__, e.message)
